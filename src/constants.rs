5cf5b893a0210336de90fb97a3ebab8d6175f98d
Ruan Barros <ruan.personal@peppo.dev>
2025-07-13 18:13:38 -0300

    feat: update constants

--- src/constants.rs
+++ src/constants.rs
@@ -1,5 +1,6 @@
-pub const DESKTOP_PATH: &str = "/home/<user>/Desktop";
-pub const APP_MENU_PATH: &str = "/home/<user>/.local/share/applications";
+pub const DESKTOP_PATH: &str = "Desktop";
+pub const APP_MENU_PATH: &str = ".local/share/applications";
 pub const STEAM_ICON_PATH: &str = "Icon=steam_icon_";
 pub const STEAM_STARTUP_WM_CLASS_PATH: &str = "StartupWMClass=";
 pub const STEAM_STARTUP_WM_CLASS_PREFIX: &str = "steam_app_";
+pub const DESKTOP: &str = "steam_app_";
